5a82d9a4f8a184d14be538280af528a679629ec3
coopergolemme <coopergolemme@gmail.com>
2023-09-22 14:54:42 -0400

    Changed UArray2

--- UArray2.h
+++ UArray2.h
@@ -1,3 +1,5 @@
+#ifndef UARRAY2_INCLUDED
+#define UARRAY2_INCLUDED
 #define T UArray2_T
 
 struct T {
@@ -16,3 +18,5 @@ int UArray2_size(T t);
 void * UArray2_at(T t, int x, int y);
 void UArray2_map_row_major(T t, void apply(int col, int row, T a,  void *cl), void *cl);
 void UArray2_map_col_major(T t, void apply(int col, int row, void *cl), void *cl);
+#undef T
+#endif
