5e43fbeb0118ccdbefe65e131e8a830ff86d690a
dkeenan01 <90722829+dkeenan01@users.noreply.github.com>
2023-09-22 15:10:58 -0400

    done

--- UArray2.h
+++ UArray2.h
@@ -9,7 +9,7 @@ void UArray2_free(T t);
 int UArray2_length(T t);
 int UArray2_width(T t);
 int UArray2_size(T t);
-void * UArray2_at(T t, int x, int y);
+void * UArray2_at(T t, int col, int row);
 void UArray2_map_row_major(T t, void apply(int col, int row, T a,  void *cl), void *cl);
 void UArray2_map_col_major(T t, void apply(int col, int row, void *cl), void *cl);
 #undef T
