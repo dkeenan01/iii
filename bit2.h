a18c30222fce4b2083431f4975418d1a108af972
dkeenan01 <90722829+dkeenan01@users.noreply.github.com>
2023-09-22 14:55:47 -0400

    bit2.h update

--- bit2.h
+++ bit2.h
@@ -1,4 +1,7 @@
-#define T Bit2
+#ifndef BIT2_INCLUDED
+#define UARRAY_INCLUDED
+#define T Bit2_T
+typedef struct T *T;
 
 
 void Bit2_new(T t, int width, int height, int size);
@@ -11,3 +14,6 @@ int Bit2_get(T t, int n);
 int Bit2_put(T t, int n);
 void Bit2_map_row_major(T t, void apply(int col, int row, void *cl), void *cl);
 void Bit2_map_col_major(T t, void apply(int col, int row, void *cl), void *cl);
+
+#undef T
+#endif
