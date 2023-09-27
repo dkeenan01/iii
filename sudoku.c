ce324e1b985dd83a1f702d806a88c14bae95599e
dkeenan01 <90722829+dkeenan01@users.noreply.github.com>
2023-09-26 20:50:56 -0400

    done

--- sudoku.c
+++ sudoku.c
@@ -2,44 +2,133 @@
 #include <stdio.h>
 #include <assert.h>
 #include <pnmrdr.h>
+#include <stdbool.h>
 #include "uarray2.h"
 
+static int two_power(int power);
 static FILE *open_or_fail(char *filename, char *mode);
-void map_to_uarray2(int i, int j, UArray2_T a, void *elem, void *image);
+UArray2_T read_input_file(FILE *file);
+void map_image(int i, int j, UArray2_T a, void *elem, void *image);
+void solve_col(int i, int j, UArray2_T a, void *elem, void *data);
+void solve_row(int i, int j, UArray2_T a, void *elem, void *data);
+bool solve_small(UArray2_T array);
+bool check_sudoku(UArray2_T sudoku);
+
+// DEBUG
 void print_elems(int i, int j, UArray2_T a, void *elem, void *cl);
-void read_input_file(FILE *file);
+
+struct solution {
+        int sum;
+        bool success;
+};
+
+typedef struct solution solution;
 
 int main(int argc, char *argv[]) {
         assert(argc == 1 || argc == 2);
 
+        bool sudoku_solved = false;
+
         if (argc == 1) {
-                // TO DO
+                
         } else if (argc == 2) {
                 FILE *fp = open_or_fail(argv[1], "rb");
-                read_input_file(fp);
+                UArray2_T array = read_input_file(fp);
+                sudoku_solved = check_sudoku(array);
+                UArray2_free(&array);
                 fclose(fp);
         }
 
-        return EXIT_SUCCESS;
+        if (sudoku_solved) {
+                return EXIT_SUCCESS;
+        } else {
+                printf("FAIL\n");
+                return EXIT_FAILURE;
+        }
 }
 
-void read_input_file(FILE *file) {
+UArray2_T read_input_file(FILE *file) {
         Pnmrdr_T image = Pnmrdr_new(file);
         Pnmrdr_mapdata image_header =  Pnmrdr_data(image);
-        assert(image_header.width == 9);
-        assert(image_header.height == 9);
+        assert(image_header.width == 9 && image_header.height == 9);
         assert(image_header.denominator == 9);
         
         UArray2_T array = UArray2_new(9, 9, 4);
         UArray2_map_row_major(array, map_to_uarray2, &image);
-        UArray2_map_row_major(array, print_elems, NULL);
-
         Pnmrdr_free(&image);
+
+        return array;
 }
 
-void map_to_uarray2(int i, int j, UArray2_T a, void *elem, void *image) {
+bool check_sudoku(UArray2_T sudoku) {
+        solution data;
+        data.sum = 0;
+        data.success = true;
+        UArray2_map_col_major(sudoku, solve_col, &data);
+        UArray2_map_row_major(sudoku, solve_row, &data);
+        data.success &= solve_small(sudoku);
+        
+        return data.success;
+}
+
+void map_image(int i, int j, UArray2_T a, void *elem, void *image) {
         (void) i, (void) j, (void) a;
         *((int*)elem) = Pnmrdr_get(*((Pnmrdr_T*) image));
+        assert(*((int*)elem) >= 1);
+        assert(*((int*)elem) <= 9);
+}
+
+void solve_col(int i, int j, UArray2_T a, void *elem, void *data) {
+        (void) a, (void) i;
+        solution* curr_data = data;
+        int* accum = &curr_data->sum;
+        int val = *((int*)elem);
+
+        val = two_power(val);
+        *accum += val;
+
+        if (j == 8) {
+                curr_data->success = curr_data->success && *accum == 511;
+                *accum = 0;
+        }
+}
+
+void solve_row(int i, int j, UArray2_T a, void *elem, void *data) {
+        (void) a, (void) j;
+        
+        solution* curr_data = data;
+        int* accum = &curr_data->sum;
+        int val = *((int*)elem);
+
+        val = two_power(val);
+        *accum += val;
+
+        if (i == 8) {
+                curr_data->success = curr_data->success && *accum == 511;
+                *accum = 0;
+        }
+}
+
+bool solve_small(UArray2_T array) {
+        for (int row = 0; row < 9; row += 3) {
+                for (int col = 0; col < 9; col += 3) {
+                        int sum = 0;
+                        for (int i = row; i < row + 3; i++) {
+                                for (int j = col; j < col + 3; j++) {
+                                        int val = *(int*)UArray2_at(array, i, j);
+                                        sum += two_power(val);
+                                }
+                        }
+                        if (sum != 511){
+                                return false;
+                        }
+                }
+        }
+        return true;
+}
+
+static int two_power(int power) {
+        return (1 << (power - 1));
 }
 
 void print_elems(int i, int j, UArray2_T a, void *elem, void *cl) {
